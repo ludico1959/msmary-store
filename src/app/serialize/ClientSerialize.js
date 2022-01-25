d91907e040bc12f05d2c07690f9be4f074ed0d98
Lucas Dias Coelho <ludico1959@gmail.com>
2022-01-25 10:56:14 -0300

    style(eslint): manually format code according eslint warnings and create some eslint rules

--- src/app/serialize/ClientSerialize.js
+++ src/app/serialize/ClientSerialize.js
@@ -1,19 +1,26 @@
 const DateFormat = require('../utils/DateFormat');
 
 const serialize = ({ _id, name, cpf, office, birthday, situation, createdAt, updatedAt }) => {
-  birthday = DateFormat.formatToRequest(birthday);
+  const formatedBirthday = DateFormat.formatToRequest(birthday);
 
-  return { _id, name, cpf, office, birthday, situation, createdAt, updatedAt };
+  return {
+    _id,
+    name,
+    cpf,
+    office,
+    birthday: formatedBirthday,
+    situation,
+    createdAt,
+    updatedAt
+  };
 };
 
-const paginateSerialize = ({ docs, limit, totalDocs, pageCounter, totalPages }) => {
-  list: docs.map(serialize);
-  limit;
-  total: totalDocs;
-  offset: pageCounter;
-  offsets: totalPages;
-
-  return { list, limit, total, offset, offsets };
-};
+const paginateSerialize = ({ docs, limit, totalDocs, pageCounter, totalPages }) => ({
+  docs: docs.map(serialize),
+  limit,
+  total: totalDocs,
+  offset: pageCounter,
+  offsets: totalPages
+});
 
 module.exports = { serialize, paginateSerialize };
