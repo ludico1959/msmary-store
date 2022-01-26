1e07cb15a8fcd3e85b31dff897d76915918159ef
Lucas Dias Coelho <ludico1959@gmail.com>
2022-01-26 14:57:25 -0300

    refactor(repository): add query parameters for finding product endpoint

--- src/app/repository/ProductRepository.js
+++ src/app/repository/ProductRepository.js
@@ -8,9 +8,14 @@ class ProductRepository {
   }
 
   async findProduct(payload) {
-    const { page = 1, limit = 20, ...query } = payload;
+    const { page = 1, limit = 20, min_price = 0, max_price = Infinity, name } = payload;
 
-    const search = payload.name ? { name: { $regex: query.name, $options: 'i' } } : { ...query };
+    const search = {
+      price: { $gte: min_price, $lte: max_price }
+    };
+
+    const searchByName = { name: { $regex: name, $options: 'i' } };
+    if (name) Object.assign(search, searchByName);
 
     return productSchema.paginate(search, {
       limit: Number(limit),
