6838e89e1926b7b6cba2c2212184040222bcd97c
Lucas Dias Coelho <ludico1959@gmail.com>
2022-01-26 21:47:45 -0300

    test(jest.setup): set it as a setup file after env in jest.config file

--- __test__/jest.setup.js
+++ __test__/jest.setup.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
+const connection = require('../src/infra/database/mongo');
 
-afterEach(async () => {
+global.beforeAll(async () => connection);
+
+global.afterEach(async () => {
   await Promise.all(
     Object.keys(mongoose.connection.collections).map(async (collection) => {
       await mongoose.connection.collections[collection].deleteMany({});
     })
   );
 });
+
+global.afterAll(async () => connection.disconnect());
--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.ts?(x)'],
+  setupFilesAfterEnv: ['./jest.setup.js']
 };
