747e93f054a90feb7e5a089550582ed06ec51cce
Lucas Dias Coelho <ludico1959@gmail.com>
2022-01-27 17:36:24 -0300

    docs(swagger): set up route for swagger in routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
 const employee = require('./employee.router');
 const product = require('./product.router');
+const swagger = require('./swagger.router');
 
 module.exports = (server) => {
   server.use((req, res, next) => {
     employee(server, new Router());
     product(server, new Router());
+    swagger(server, new Router());
     next();
   });
 };
